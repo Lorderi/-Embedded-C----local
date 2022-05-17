f7adb773c996fa3b8caf83a6af8fcec9ed872f7e
Artemij <92867907+Lorderi@users.noreply.github.com>
2022-05-17 14:46:56 +0700

    Update main4.c

--- 1 массивы/main4.c
+++ 1 массивы/main4.c
@@ -1,4 +1,4 @@
-#include stdio.h
+#include <stdio.h>
 #define N 7
 
 int num = 1;
@@ -13,36 +13,36 @@ int main(void)
 
 	int matrix[N][N], i = 0, j = 0, nomm = 0;
 
-	while (num = (N  N)){
+	while (num <= (N * N)){
 		i = nomm;
-		for (j = nomm; j  N - nomm; j++) {
+		for (j = nomm; j < N - nomm; j++) {
 			matrix[i][j] = plus();
 		}
 		j = N - nomm - 1;
-		for (i = nomm + 1; i  N - nomm - 1; i++) {
+		for (i = nomm + 1; i < N - nomm - 1; i++) {
 			matrix[i][j] = plus();
 		}
-		if (num  (N  N)) {
+		if (num > (N * N)) {
 			break;
 		}
 		i = N - nomm - 1;
-		for (j = N - nomm - 1; j = nomm; j--) {
+		for (j = N - nomm - 1; j >= nomm; j--) {
 			matrix[i][j] = plus();
 		}
 		j = nomm;
-		for (i = N - nomm - 2; i  nomm; i--) {
+		for (i = N - nomm - 2; i > nomm; i--) {
 			matrix[i][j] = plus();
 		}
 		nomm++;
 	}
 	
 
-	printf(Выходящий массивnn);
-	for (i = 0; i  N; i++) {
-		for (j = 0; j  N; j++) {
-			printf(%d , matrix[i][j]);
+	printf("Выходящий массив\n\n");
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < N; j++) {
+			printf("%d ", matrix[i][j]);
 		}
-		printf(n);
+		printf("\n");
 	}
 
 
